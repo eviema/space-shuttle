9c17b1b46f18ddbc23ca609afff8eddf0a43940f
Evie Ma <yfma89@gmail.com>
2017-12-02 15:36:29 +1100

    modify model class Course

--- src/main/java/au/com/jiangren/spaceshuttle/model/Course.java
+++ src/main/java/au/com/jiangren/spaceshuttle/model/Course.java
@@ -1,14 +1,6 @@
 package au.com.jiangren.spaceshuttle.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
@@ -16,20 +8,26 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY, TABLE, AUTO
     private Long id;
-
+    @Column(name = "course_name", nullable = false, length = 30)
     private String name;
-
+    @Column(name = "course_code", nullable = false, length = 20)
+    private String code;
+    @Column(name = "course_subtitle")
+    private String subtitle;
+    @Column(name = "course_desc", nullable = false, length = 50)
     private String description;
-
+    @Column(name = "course_location")
+    private String location;
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(name = "course_instructor", joinColumns = @JoinColumn(name = "instructor_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "course_id", referencedColumnName = "id"))
     private Set<Instructor> instructors;
-
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(name = "course_category", joinColumns = @JoinColumn(name = "course_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "category_id", referencedColumnName = "id"))
     private Set<Category> categories;
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<Class> classes;
 
     public Course() {
 
@@ -56,12 +54,36 @@ public class Course {
         this.name = name;
     }
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     public Set<Instructor> getInstructors() {
@@ -80,5 +102,11 @@ public class Course {
         this.categories = categories;
     }
 
+    public Set<Class> getClasses() {
+        return classes;
+    }
 
+    public void setClasses(Set<Class> classes) {
+        this.classes = classes;
+    }
 }
