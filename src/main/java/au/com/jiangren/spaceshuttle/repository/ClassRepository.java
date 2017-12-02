2a5612c6a38ca2291e8d3fcfaeb293f6ddc2270e
Evie Ma <yfma89@gmail.com>
2017-12-02 20:03:19 +1100

    add ClassRepository

---
+++ src/main/java/au/com/jiangren/spaceshuttle/repository/ClassRepository.java
@@ -0,0 +1,8 @@
+package au.com.jiangren.spaceshuttle.repository;
+
+import au.com.jiangren.spaceshuttle.model.Class;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassRepository extends JpaRepository<Class, Long> {
+
+}
